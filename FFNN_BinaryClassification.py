22631bd486a76a5ded2fd83ef84b64d0a1052173
Flaviu-Pop <flaviu_v@yahoo.com>
2025-01-02 15:22:44 +0200

    Added basic functionality

---
+++ FFNN_BinaryClassification.py
@@ -0,0 +1,305 @@
+import copy
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import pandas as pd
+import time
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+
+
+
+pd.set_option('display.max_columns', None)
+
+
+path = 'C:\Learning\Computer Science\Machine Learning\\03 My Projects\\02 Codes\\01 BinaryClassification\My_Learning_PyTorch_FCNN_BinaryClassification\MyProject_Stats_PyTorch_BinaryClassification_Churn_ANN_01\Churn_Modelling.csv'
+dataset = pd.read_csv(path)
+
+
+
+
+
+########################################### ----- DATA STATISTICS ----- ################################################
+
+print('\n\n\nThe first ten rows\observations are:\n')
+print(dataset.head(10))
+
+
+print("\n\n\nThe statistics summary of the dataset is:\n")
+print(dataset.describe())
+
+
+print("\n\n\nThe Object Datatypes are:\n")
+print(dataset.dtypes)
+
+
+num_zeros = (dataset[['RowNumber', 'CustomerId', 'CreditScore', 'Age', 'Tenure',
+                      'Balance', 'NumOfProducts', 'HasCrCard', 'IsActiveMember', 'EstimatedSalary', 'Exited']] == 0).sum()
+print("\n\n\nThe number of zeros from each column:\n")
+print(num_zeros)
+
+
+print('\n\n\nThe number of NaN for each column is:\n')
+print(dataset.isnull().sum())
+
+
+
+
+
+####################################### ----- DATA PRE-PRPCESSING ----- ################################################
+
+# We teke the Descriptive Features and Target Feature, respectively
+X = dataset.iloc[:, 3:-1].values  #Descriptive Feature == Independent Variable
+y = dataset.iloc[:, -1].values    #Target Feature == Dependent Variable
+
+
+# We make the corresponding encodings
+
+# Encoding the "Gender" column ----- LabelEncoder()
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+
+# Encoding the "Geography" column ----- OneHotEncoder()
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+
+# We split the Dataset into Training Set and Test Set, respectively
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+
+# We do Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+
+# We convert the datasets into (PyTorch) Tensors
+X_train = torch.FloatTensor(X_train)
+X_test = torch.FloatTensor(X_test)
+y_train = torch.Tensor(y_train)
+y_test = torch.Tensor(y_test)
+
+
+
+print("\n\n\nThe shapes are:")
+print(f"X_train: {X_train.size()}")
+print(f"X_Test: {X_test.size()}")
+print(f"y_train: {y_train.size()}")
+print(f"y_test has: {y_test.size()}")
+
+
+
+
+
+##################################  THE ARCHITECTURE --- FCNN ##########################################################
+
+class FFNN_BinaryClassification(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.linearLayer1 = torch.nn.Linear(in_features=12, out_features=20)
+        self.relu1 = torch.nn.ReLU()
+        self.linearLayer2 = torch.nn.Linear(in_features=20, out_features=30)
+        self.relu2 = torch.nn.ReLU()
+        self.linearLayer3 = torch.nn.Linear(in_features=30, out_features=50)
+        self.relu3 = torch.nn.ReLU()
+        self.linearLayer4 = torch.nn.Linear(in_features=50, out_features=100)
+        self.relu4 = torch.nn.ReLU()
+        self.linearLayer5 = torch.nn.Linear(in_features=100, out_features=1)
+        self.sigmoid = torch.nn.Sigmoid()
+
+
+
+    def forward(self, x):
+        x = self.linearLayer1(x)
+        x = self.relu1(x)
+
+        x = self.linearLayer2(x)
+        x = self.relu2(x)
+
+        x = self.linearLayer3(x)
+        x = self.relu3(x)
+
+        x = self.linearLayer4(x)
+        x = self.relu4(x)
+
+        x = self.linearLayer5(x)
+        x = self.sigmoid(x)
+
+        return x
+
+
+
+
+
+###################################### THE TRAINING PROCEDURE ##########################################################
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+def compute_accuracy(model, inputs, labels):
+    # It computes the accuracy of the <model> on the dataset <inputs,labels>
+
+    total_correct = 0
+
+    # Set the model/data to GPU is available
+    model = model.to(device)
+    inputs = inputs.to(device)
+    labels = labels.to(device)
+
+    model.eval() # Set the model to evaluation mode
+
+    outputs = model(inputs)
+    outputs = torch.round(outputs)
+
+    outputs_shape = list(outputs.size())
+    reshape_index_0 = outputs_shape.__getitem__(0)
+    outputs = outputs.reshape([reshape_index_0])
+
+    total_correct = (outputs == labels).sum()
+
+    return total_correct/reshape_index_0
+
+
+
+
+
+def train(model, train_inputs, train_labels, test_inputs, test_labels, batch_size, num_epochs, criterion, optimizer):
+    print("\n\n\n--- Now: The Training Process ---\n")
+
+    # Set the model/data to GPU if available
+    model = model.to(device)
+    train_inputs, train_labels = train_inputs.to(device), train_labels.to(device)
+
+    train_size = (train_inputs.size())[0]
+    batches = torch.arange(0, train_size, batch_size)
+
+    best_accuracy = -np.inf
+    best_weights = None
+    best_epoch = 0
+
+    # The Training-Loop
+    for epoch in range(num_epochs):
+        start_time = time.perf_counter()
+
+        model.train()   # Set the model to training mode
+
+        total_loss = 0
+        for start_batch in batches:
+            # Take a batch
+            batch_inputs = train_inputs[start_batch : start_batch + batch_size]
+            batch_labels = train_labels[start_batch : start_batch + batch_size]
+
+            # Forward and Backward Passes
+            optimizer.zero_grad()
+            batch_outputs = model(batch_inputs)
+            batch_outputs = torch.squeeze(batch_outputs, 1)
+            batch_loss = criterion(batch_outputs, batch_labels)
+            batch_loss.backward()
+            optimizer.step()
+
+            total_loss += batch_loss
+
+
+        # Take the model('s weights) with the best accuracy
+        test_accuracy = compute_accuracy(model, test_inputs, test_labels)
+        if test_accuracy > best_accuracy:
+            best_accuracy = test_accuracy
+            best_weights = copy.deepcopy(model.state_dict())
+            best_epoch = epoch + 1
+
+        end_time = time.perf_counter()
+        epoch_time = end_time - start_time
+
+        print(f"Epoch = {epoch + 1} ===> Loss = {total_loss: .3f} ===> Time = {epoch_time: .3f} ===> Test Accuracy = {test_accuracy: .4f} ===> Best Accuracy = {best_accuracy: .4f} at Epoch {best_epoch}")
+
+    # Set the model('s weights) with the best accuracy
+    model.load_state_dict(best_weights)
+
+
+    print("\n\n\nJust checking ...")
+    final_accuracy = compute_accuracy(model, test_inputs, test_labels)
+    print(f"\nThe Accuracy of the Final Models is: {final_accuracy}")
+
+
+
+
+
+################################################# ---  MAIN() ---  #####################################################
+
+ffnn = FFNN_BinaryClassification()
+
+batch_size = 10
+number_of_epochs = 150
+criterion = torch.nn.BCELoss()
+optimizer = torch.optim.Adam(ffnn.parameters(), lr=0.001)
+
+train(model=ffnn, train_inputs=X_train, train_labels=y_train, test_inputs=X_test, test_labels=y_test, batch_size=batch_size, num_epochs=number_of_epochs, criterion=criterion, optimizer=optimizer)
+
+
+
+
+
+##################################### -----  PREDICTION/INFERENCE ----- ################################################
+
+print("\n\n\n----- Now: We do some predictions, some inference -----")
+
+ffnn.eval() # Set the model to evaluation mode
+
+prediction_01 = ffnn(torch.FloatTensor(sc.transform([[1,0,0,600,1,40,3,60000,2,1,1,50000]])))
+print("\nThe probability is: " + str(prediction_01.item()))
+print("The answer is: " + str((prediction_01 > 0.5).item()))
+
+prediction_02 = ffnn(torch.FloatTensor(sc.transform([[1,0,0,619,0,42,2,0,1,1,1,101348.88]])))
+print("\nThe probability is: " + str(prediction_02.item()))
+print("The answer is: " + str((prediction_02 > 0.5).item()))
+print("The true is: <exit = 1>")
+
+prediction_03 = ffnn(torch.FloatTensor(sc.transform([[0,0,1,608,0,41,1,83807.86,1,0,1,112542.58]])))
+print("\nThe probability is: " + str(prediction_03.item()))
+print("The answer is: " + str((prediction_03 > 0.5).item()))
+print("The true is: <exit = 0>")
+
+prediction_04 = ffnn(torch.FloatTensor(sc.transform([[1,0,0,502,0,42,8,159660.80,3,1,0,113931.57]])))
+print("\nThe probability is: " + str(prediction_04.item()))
+print("The answer is: " + str((prediction_04 > 0.5).item()))
+print("The true is: <exit = 1>")
+
+prediction_05 = ffnn(torch.FloatTensor(sc.transform([[0,1,0,653,1,58,1,132602.90,1,1,0,5097.67]])))
+print("\nThe probability is: " + str(prediction_05.item()))
+print("The answer is: " + str((prediction_05 > 0.5).item()))
+print("The true is: <exit = 1>")
+
+prediction_06 = ffnn(torch.FloatTensor(sc.transform([[1,0,0,411,1,29,0,59697.17,2,1,1,53483.21]])))
+print("\nThe probability is: " + str(prediction_06.item()))
+print("The answer is: " + str((prediction_06 > 0.5).item()))
+print("The true is: <exit = 0>")
+
+prediction_07 = ffnn(torch.FloatTensor(sc.transform([[0,0,1,591,0,39,3,0,3,1,0,140469.4]])))
+print("\nThe probability is: " + str(prediction_07.item()))
+print("The answer is: " + str((prediction_07 > 0.5).item()))
+print("The true is: <exit = 1>")
+
+prediction_08 = ffnn(torch.FloatTensor(sc.transform([[0,1,0,585,1,36,5,146051,2,0,0,86424.57]])))
+print("\nThe probability is: " + str(prediction_08.item()))
+print("The answer is: " + str((prediction_08 > 0.5).item()))
+print("The true is: <exit = 0>")
+
+prediction_09 = ffnn(torch.FloatTensor(sc.transform([[0,1,0,655,1,41,8,125562,1,0,0,164040.9]])))
+print("\nThe probability is: " + str(prediction_09.item()))
+print("The answer is: " + str((prediction_09 > 0.5).item()))
+print("The true is: <exit = 1>")
+
+prediction_10 = ffnn(torch.FloatTensor(sc.transform([[1,0,0,646,0,46,4,0,3,1,0,93251.42]])))
+print("\nThe probability is: " + str(prediction_10.item()))
+print("The answer is: " + str((prediction_10 > 0.5).item()))
+print("The true is: <exit = 1>")
---
+++ FFNN_NonBinaryClassification.py
@@ -0,0 +1,255 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import pandas as pd
+import numpy as np
+import time
+import copy
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+
+
+pd.set_option('display.max_columns', None)
+torch.set_printoptions(precision=5, sci_mode=False)
+
+
+# We load the data from a csv file
+path = 'C:\Learning\Computer Science\Machine Learning\\03 My Projects\\01 Datasets\\02 NonBinaryClassification\kaggle_diagnosed_cbc_data_v4.csv'
+dataset = pd.read_csv(path)
+
+
+
+
+
+#################################### ----- DATA STATISTICS ----- #######################################################
+
+print("\n\n\nThe first 10 rows from dataset are: \n")
+print(dataset.head(10))
+
+
+print("\n\n\nThe Statistics of all columns (DFs and TFs) are: \n")
+print(dataset.describe())
+
+
+print("\n\n\nThe number of NaN's corresponding to each column is: \n")
+print(dataset.isnull().sum())
+
+
+print("\n\n\nThe Column's DataTypes are: \n")
+print(dataset.dtypes)
+
+
+print("\n\n\nThe Target Levels of the dataset are: \n")
+print(dataset['Diagnosis'].unique())
+
+
+print("\n\n\nThe number of Target Levels Instances are: \n")
+print(dataset['Diagnosis'].value_counts())
+
+
+
+
+
+################################ ----- DATA PRE-PROCESSING ----- #######################################################
+
+# We split the Independent Features from the Dependent Features
+X = dataset.iloc[:, 0 : -1].values
+y = dataset.iloc[:, -1].values
+
+
+y = y.reshape(-1, 1)
+
+
+
+# We convert the TFs to One-Hot-Encoding representation
+encoder = OneHotEncoder(sparse_output=False)
+y = encoder.fit_transform(y)
+
+
+
+# We split tha Dataset into Training Set and Test Set, respectively
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=14)
+
+
+
+# Feature Scaling ----- Normalization
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+
+# Convert the data into (PyTorch) Tensors
+X_train = torch.Tensor(X_train)
+X_test = torch.Tensor(X_test)
+y_train = torch.Tensor(y_train)
+y_test = torch.Tensor(y_test)
+
+
+
+print("\n\n\nThe shapes are:")
+print(f"X_train: {X_train.size()}")
+print(f"X_Test: {X_test.size()}")
+print(f"y_train: {y_train.size()}")
+print(f"y_test has: {y_test.size()}")
+
+
+
+
+
+#################################### -----  THE ARCHITECTURE  ------ ###################################################
+
+class FFNN_NonBinaryClassification(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.linearLayer1 = nn.Linear(in_features=14, out_features=10)
+        self.activation1 = nn.ReLU()
+
+        self.linearLayer2 = nn.Linear(in_features=10, out_features=50)
+        self.activation2 = nn.ReLU()
+
+        self.linearLayer3 = nn.Linear(in_features=50, out_features=100)
+        self.activation3 = nn.ReLU()
+
+        self.linearLayer4 = nn.Linear(in_features=100, out_features=150)
+        self.activation4 = nn.ReLU()
+
+        self.outputLayer = nn.Linear(in_features=150, out_features=9)
+        self.activationOutput = nn.Softmax(dim=1)
+
+
+
+    def forward(self, x):
+        x = self.linearLayer1(x)
+        x = self.activation1(x)
+
+        x = self.linearLayer2(x)
+        x = self.activation2(x)
+
+        x = self.linearLayer3(x)
+        x = self.activation3(x)
+
+        x = self.linearLayer4(x)
+        x = self.activation4(x)
+
+        x = self.outputLayer(x)
+        x = self.activationOutput(x)
+
+        return x
+
+
+
+
+##################################### ----- TRAINING PROCEDURE ----- ###################################################
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+def compute_accuracy(model, inputs, labels):
+    # It computes the accuracy of the <model> on the dataset <inputs,labels>
+
+    # Set the model/data to GPU is available
+    model = model.to(device)
+    inputs, labels = inputs.to(device), labels.to(device)
+
+    model.eval()     # Set the model to evaluation mode
+
+    outputs = model(inputs)
+    acc = (torch.argmax(outputs, 1) == torch.argmax(labels, 1)).float().mean()
+    return acc
+
+
+
+def train(model, train_inputs, train_labels, test_inputs, test_labels, batch_size, num_epochs, criterion, optimizer):
+    print("\n\n\n ... The training process ...\n")
+
+    # Set the model/data to GPU if available
+    model = model.to(device)
+    train_inputs, train_labels = train_inputs.to(device), train_labels.to(device)
+
+    train_size = (train_inputs.size())[0]
+    batches = torch.arange(0, train_size, batch_size)
+
+    best_accuracy = -np.inf
+    best_weights = None
+    best_epoch = 0
+
+    for epoch in range(num_epochs):
+        start_time = time.perf_counter()
+        model.train()
+
+        total_loss = 0
+        for start_batch in batches:
+            # Take a batch
+            X_batch = train_inputs[start_batch : start_batch + batch_size]
+            y_batch = train_labels[start_batch : start_batch + batch_size]
+
+            # Forward and Backward Passes
+            optimizer.zero_grad()
+            batch_outputs = model(X_batch)
+            batch_loss = criterion(batch_outputs, y_batch)
+            batch_loss.backward()
+            optimizer.step()
+
+            total_loss += batch_loss
+
+
+        # Compute the model's accuracy on test set and record the model's wigths corresponding to the best accuracy
+        test_accuracy = compute_accuracy(model, test_inputs, test_labels)
+        if test_accuracy > best_accuracy:
+            best_accuracy = test_accuracy
+            best_weights = copy.deepcopy(model.state_dict())
+            best_epoch = epoch + 1
+
+        end_time = time.perf_counter()
+        epoch_time = end_time - start_time
+
+
+        print(f"Epoch = {epoch + 1} ===> Loss = {total_loss: .3f} ===> Time = {epoch_time: .3f} ===> Test Accuracy = {test_accuracy: .4f} ===> Best Accuracy = {best_accuracy} amt epoch {best_epoch}")
+
+    # Set the model('s weights) with the best accuracy
+    model.load_state_dict(best_weights)
+
+
+
+
+
+################################################# -----  MAIN() ----- ##################################################
+
+ffnn = FFNN_NonBinaryClassification()
+
+batch_size = 10
+number_of_epochs = 50
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(ffnn.parameters(), lr=0.001)
+
+train(model=ffnn, train_inputs=X_train, train_labels=y_train, test_inputs=X_test, test_labels=y_test, batch_size=batch_size, num_epochs=number_of_epochs, criterion=criterion, optimizer=optimizer)
+
+
+
+
+
+############################################## ----- PREDICTION ----- ##################################################
+
+print("\n\n\n----- Now we do some Inference --- some Prediction -----")
+
+ffnn.eval()
+
+prediction_01 = ffnn(torch.FloatTensor(scaler.transform([[10, 43.2, 50.1, 4.3, 5, 2.77, 7.3, 24.2, 87.7, 26.3, 30.1, 189, 12.5, 0.17]])))
+print("\nThe probabilities are: " + str(prediction_01))
+print("The answer is: " + str((torch.argmax(prediction_01)).item() +1))
+print("The true is: <index class = 6>")
+
+prediction_02 = ffnn(torch.FloatTensor(scaler.transform([[7.2, 30.7, 60.7, 2.2, 4.4, 3.97, 9, 30.5, 77, 22.6, 29.5, 148, 14.3, 0.14]])))
+print("\nThe probabilities are: " + str(prediction_02))
+print("The answer is: " + str((torch.argmax(prediction_02)).item() + 1))
+print("The true is: <index class = 2>")
+
+prediction_03 = ffnn(torch.FloatTensor(scaler.transform([[5.2, 19.7, 72.4, 1, 3.8, 4.85, 13.2, 41, 84.7, 27.2, 32.1, 181, 10, 0.15]])))
+print("\nThe probabilities are: " + str(prediction_03))
+print("The answer is: " + str((torch.argmax(prediction_03)).item() +1))
+print("The true is: <index class = 1>")
